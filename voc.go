9ab9d522d5c12dea06fe19ccd831559cfd4e81cb
owenramber1989 <owenramber1989@163.com>
2023-05-01 19:46:02 +0800

    <init>: <the first commit>

---
+++ voc.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Word struct {
+	id         int
+	word       string
+	meaning    string
+	importance string
+}
+
+func main() {
+	// 设置数据库连接信息
+	user, password, host, database, err := readConfig("voc.conf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(os.Args) < 2 {
+		log.Fatal("too few arguments\n")
+	}
+	operation := os.Args[1]
+	/*
+		user := "root"
+		password := ""
+		host := "localhost"
+		database := "vocabulary"
+	*/
+	// 构建数据源名称 (DSN)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", user, password, host, database)
+
+	// 连接到数据库
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	defer db.Close()
+
+	// 检查数据库连接
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping the database:", err)
+	}
+
+	fmt.Println("Successfully connected to the database")
+	fmt.Println("")
+	if operation == "-h" {
+		fmt.Printf("insert a word:      voc -iw word meaning importance\n\n")
+		fmt.Printf("insert a phrase:    voc -ip phrase meaning importance\n\n")
+		fmt.Printf("update importance:  voc -u word importance\n\n")
+		fmt.Printf("query a word:       voc -q word\n\n")
+		fmt.Printf("show words:         voc -sw\n\n")
+		fmt.Printf("show phrases:       voc -sp\n\n")
+		fmt.Printf("delete entries:     voc -d table id offset\n\n")
+		fmt.Printf("help:               voc -h\n\n")
+	}
+	if operation == "-iw" {
+		if len(os.Args) != 5 {
+			fmt.Println("Usage: voc -iw word meaning importance")
+			return
+		}
+		word := os.Args[2]
+		meaning := os.Args[3]
+		importanceStr := os.Args[4]
+		importance, err := strconv.Atoi(importanceStr)
+		if err != nil {
+			log.Fatal("Failed to convert importance to integer:", err)
+		}
+		insertedID, err := insertEntry(db, importance, "words", word, meaning)
+		if err != nil {
+			log.Fatal("Failed to insert entry:", err)
+		}
+		fmt.Printf("Successfully inserted entry with id: %d", insertedID)
+	}
+
+	if operation == "-ip" {
+		if len(os.Args) != 5 {
+			fmt.Println("Usage: voc -ip phrase meaning importance")
+			return
+		}
+		phrase := os.Args[2]
+		meaning := os.Args[3]
+		importanceStr := os.Args[4]
+		importance, err := strconv.Atoi(importanceStr)
+		if err != nil {
+			log.Fatal("Failed to convert importance to integer:", err)
+		}
+		insertedID, err := insertEntry(db, importance, "phrases", phrase, meaning)
+		if err != nil {
+			log.Fatal("Failed to insert entry:", err)
+		}
+		fmt.Printf("Successfully inserted entry with id: %d", insertedID)
+	}
+	if operation == "-u" {
+		if len(os.Args) != 4 {
+			fmt.Println("usage: voc -u word importance")
+			return
+		}
+		insertion := os.Args[2]
+		importanceStr := os.Args[3]
+		importance, er := strconv.Atoi(importanceStr)
+		if er != nil {
+			log.Fatal("Failed to convert importance to integer:", err)
+		}
+		err := updateImportance(db, "words", insertion, importance)
+		if err != nil {
+			log.Fatal("Failed to update importance:", err)
+		}
+		fmt.Println("Successfully updated importance")
+	}
+	if operation == "-q" {
+		word := os.Args[2]
+		meaning, importance, err := queryWords(db, word)
+		if err != nil {
+			log.Fatal("Failed to query word:", err)
+		}
+		fmt.Printf("%s的意思是: %s\n", word, meaning)
+		fmt.Printf("重要性: %d\n", importance)
+	}
+	if operation == "-sw" {
+		err := show(db, "words")
+		if err != nil {
+			log.Fatal("Failed to show words:", err)
+		}
+	}
+	if operation == "-sp" {
+		err := show(db, "phrases")
+		if err != nil {
+			log.Fatal("Failed to show phrases:", err)
+		}
+	}
+	if operation == "-d" {
+		table := os.Args[2]
+		idStr := os.Args[3]
+		offStr := os.Args[4]
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			log.Fatal("Failed to convert ID to integer:", err)
+		}
+		off, err := strconv.Atoi(offStr)
+		if err != nil {
+			log.Fatal("Failed to convert offset to integer:", err)
+		}
+		err = delete(db, table, id, off)
+		if err != nil {
+			log.Fatal("Failed to delete entries:", err)
+		}
+	}
+
+}
+
+func insertEntry(db *sql.DB, importance int, table, insertion, meaning string) (int64, error) {
+	replace := "entry"
+	if table == "words" {
+		replace = "word"
+	}
+	query := fmt.Sprintf("INSERT INTO %s (%s, meaning, importance) VALUES (?, ?, ?)", table, replace)
+	result, err := db.Exec(query, insertion, meaning, importance)
+
+	if err != nil {
+		return 0, err
+	}
+
+	insertedID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return insertedID, nil
+}
+
+func queryWords(db *sql.DB, word string) (string, int, error) {
+	var meaning string
+	var importance int
+	err := db.QueryRow("SELECT meaning, importance FROM words WHERE word = ?", word).Scan(&meaning, &importance)
+
+	if err != nil {
+		return "", -1, err
+	}
+
+	return meaning, importance, nil
+}
+
+func updateImportance(db *sql.DB, table string, insertion string, importance int) error {
+	column := "word"
+	if table == "phrases" {
+		column = "entry"
+	}
+	query := fmt.Sprintf("UPDATE %s SET importance = ? WHERE %s = ?", table, column)
+	_, err := db.Exec(query, importance, insertion)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func show(db *sql.DB, table string) error {
+	// Prepare the query string
+	query := fmt.Sprintf("SELECT * FROM %s", table)
+
+	// Execute the query
+	rows, err := db.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// Print the column names with specified widths
+	fmt.Printf("%-4s\t%-30s\t%-30s\t%10s\n", "ID", "Word/Phrase", "Meaning", "Importance")
+
+	// Iterate through the rows and print the data
+	for rows.Next() {
+		var id int
+		var entry, meaning string
+		var importance int
+
+		err := rows.Scan(&id, &entry, &meaning, &importance)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%-4d\t%-30s\t%-30s\t%-5d\n", id, entry, meaning, importance)
+	}
+
+	// Check for errors during iteration
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func delete(db *sql.DB, table string, id int, off int) error {
+	// Prepare the delete statement
+	query := fmt.Sprintf("DELETE FROM %s WHERE id >= ? AND id <= ?", table)
+
+	// Execute the delete statement
+	_, err := db.Exec(query, id, id+off)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Deleted entries with ID between %d and %d in table %s.\n", id, id+off, table)
+	return nil
+}
+
+func readConfig(filename string) (string, string, string, string, error) {
+	config, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", "", "", "", fmt.Errorf("Failed to read the config file: %v", err)
+	}
+
+	lines := strings.Split(string(config), "\n")
+
+	var user, password, host, database string
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+
+		keyValue := strings.Split(line, ":")
+		key := strings.TrimSpace(keyValue[0])
+		value := strings.TrimSpace(keyValue[1])
+
+		switch key {
+		case "user":
+			user = value
+		case "password":
+			password = value
+		case "host":
+			host = value
+		case "database":
+			database = value
+		default:
+			return "", "", "", "", fmt.Errorf("Unknown key in config file: %s", key)
+		}
+	}
+
+	return user, password, host, database, nil
+}
